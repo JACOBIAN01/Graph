6b9c12ca8dde99d05101eeaf58cb544f819ed4b7
JACOBIAN01 <subhadeepghorai23@gmail.com>
2025-07-03 23:38:19 +0530

    Update

--- CycleDetectionDirectedDFS.java
+++ CycleDetectionDirectedDFS.java
@@ -1,10 +1,55 @@
-
+import java.util.*;
 public class CycleDetectionDirectedDFS {
 
-    public static void main(String[] args){
-        
+    static void addEdge(List<List<Integer>>graph,int u,int v){
+        graph.get(u).add(v);//Directed Graph only one direction
     }
 
+    // static boolean dfs(int node,List<List<Integer>>graph,boolean[] visited, boolean[]recStack ){
+
+    //     visited[node] = true;
+    //     recStack[node] = true;
+
+    //     for(int neighbor:graph.get(node)){
+    //         if(!visited[node]){
+    //             if(dfs(neighbor,graph,visited,recStack)){
+
+    //             }
+    //         }
+    //     }
+    // }
+    public static void main(String[] args){
+        int n= 4;
+
+        List<List<Integer>>adjList = new ArrayList<>();
+        for(int i =0 ;i<n;i++){
+            adjList.add(new ArrayList<>());
+        }
+        // Add directed edges
+        addEdge(adjList, 0, 1);
+        addEdge(adjList, 1, 2);
+        addEdge(adjList, 2, 3);
+        addEdge(adjList, 3, 1); // This edge creates a cycle
+
+        boolean[] visited = new boolean[n];
+        boolean[] recStack = new boolean[n];
+
+        boolean hasCycle = false;
+
+        // for(int i=0;i<n;i++){
+        //     if(!visited[i]){
+        //         if(dfs(i,adjList,visited,recStack)){
+        //             hasCycle=true;
+        //             break;
+        //         }
+        //     }
+        // }
 
 
+
+
+
+
+  
+    }
 }
